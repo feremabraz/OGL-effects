5c048f8eae454b2dcd283a4587f81bf968afa306
Fernando Braz <hello@fernandobraz.online>
2025-06-15 14:24:49 +0100

    refactor: layout changes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import { AppSidebar } from '@/components/app-sidebar';
-import { SiteHeader } from '@/components/site-header';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { EffectsList } from '@/components/effects-list';
 
-const formatEffectName = (name: string) => {
-  return name
-    .split('-')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
-};
-
 export default function Home() {
-  return (
-    <SidebarProvider className="h-screen">
-      <AppSidebar variant="inset" />
-      <SidebarInset className="h-full flex flex-col">
-        <SiteHeader />
-        <EffectsList className="mt-12" />
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <EffectsList className="mt-72" />;
 }
